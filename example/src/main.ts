b4974140e4ff32bbb81633287f457c6908691303
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2022-02-10 09:52:50 +0100

    Migrate to tsup

--- example/src/main.ts
+++
@@ -1,9 +0,0 @@
-import longText from '../../tests/files/complex-raw.docsy?raw';
-import { DocsyParser } from '../../dist';
-
-console.time('parse');
-const result = DocsyParser.parseDocument(longText);
-console.timeEnd('parse');
-
-const app = document.getElementById('app')!;
-app.innerHTML = `<pre>${JSON.stringify(result.document, null, 2)}</pre>`;
